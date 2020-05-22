a9665d493a6be7198cf427f72e0d28a3ad43ab36
う樱雪こ <41228270+thisisloves@users.noreply.github.com>
2020-05-22 16:50:29 +0800

    Add files via upload

---
+++ precache-manifest.aec1c4917fe97e9ce82ca0deea73b7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2f09b12594db0f81b5b22f5c38767e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9f75a0f4fdfcf30b3eb",
+    "url": "/static/css/2.dd393451.chunk.css"
+  },
+  {
+    "revision": "32a8661b9fc75e75653a",
+    "url": "/static/css/main.af2bfc64.chunk.css"
+  },
+  {
+    "revision": "d9f75a0f4fdfcf30b3eb",
+    "url": "/static/js/2.3fb08c08.chunk.js"
+  },
+  {
+    "revision": "32a8661b9fc75e75653a",
+    "url": "/static/js/main.136c3ec8.chunk.js"
+  },
+  {
+    "revision": "ffee039ff2d0523426a5",
+    "url": "/static/js/runtime~main.8b698ba5.js"
+  }
+]);
